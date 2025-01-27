f2fad2081edf146431d8240499d0da3e43df37b8
isroil01 <isroiljonrustamov25@gmail.com>
2025-01-27 14:43:30 +0900

    changed

--- src/app/main.py
+++ src/app/main.py
@@ -1,2 +1,12 @@
+import uvicorn
+from fastapi import FastAPI
+
+from app.setting import Settings
+
+settings = Settings()
+
+app = FastAPI()
+
+
 def main():
-    print("Hello world")
+    uvicorn.run("app.main:app", host=settings.host_address, port=settings.port, log_level="debug")
